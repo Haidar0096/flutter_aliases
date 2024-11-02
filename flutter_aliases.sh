40e5a9f432e432b0a4e4e87157cf0edab95da433
Haidar Mehsen <49842031+Haidar0096@users.noreply.github.com>
2024-11-02 20:41:47 +0200

    Update flutter_aliases.sh

--- flutter_aliases.sh
+++ flutter_aliases.sh
@@ -1,24 +1,55 @@
 #!/bin/bash
 
-## To use this file on macos, you need to add the following line to your .zshrc file
-## source ~/path/to/flutter_aliases.sh
+# Define core Flutter commands without alias prefix
+CMD_DRBRB="dart run build_runner build --delete-conflicting-outputs"
+CMD_DRBRW="dart run build_runner watch --delete-conflicting-outputs"
+CMD_FPG="flutter pub get"
+CMD_FBAPKR="flutter build apk --release --split-per-abi"
+CMD_FBAPK64R="flutter build apk --release --target-platform=android-arm64"
+CMD_FBAB="flutter build appbundle --release"
+CMD_FC="flutter clean"
+CMD_DF="dart format ."
+CMD_FGL="flutter gen-l10n"
+CMD_FCPGGL="flutter clean && flutter pub get && flutter gen-l10n"
+CMD_FRDE="flutter run -d emulator-5554"
+CMD_DFX="dart fix --apply"
+CMD_FD="flutter doctor -v"
 
-##### Useful Flutter commands aliases #####
-alias drbrb='echo "running dart run build_runner build --delete-conflicting-outputs"; dart run build_runner build --delete-conflicting-outputs'
+# Function to run commands
+run_command() {
+    echo "running $1"
+    eval "$1"
+}
 
-alias drbrw='echo "running dart run build_runner watch --delete-conflicting-outputs"; dart run build_runner watch --delete-conflicting-outputs'
+# Regular Flutter aliases
+alias drbrb='run_command "$CMD_DRBRB"'
+alias drbrw='run_command "$CMD_DRBRW"'
+alias fpg='run_command "$CMD_FPG"'
+alias fbapkr='run_command "$CMD_FBAPKR"'
+alias fbapk64r='run_command "$CMD_FBAPK64R"'
+alias fbab='run_command "$CMD_FBAB"'
+alias fc='run_command "$CMD_FC"'
+alias df='run_command "$CMD_DF"'
+alias fgl='run_command "$CMD_FGL"'
+alias fcpggl='run_command "$CMD_FCPGGL"'
+alias frde='run_command "$CMD_FRDE"'
+alias dfx='run_command "$CMD_DFX"'
+alias fd='run_command "$CMD_FD"'
 
-alias flc='echo "running flutter clean"; flutter clean'
+# FVM Command prefix
+fvm_prefix="fvm "
 
-alias fpg='echo "running flutter pub get"; flutter pub get'
-
-alias fbapk='echo "running flutter build apk --release --split-per-abi"; flutter build apk --release --split-per-abi'
-
-alias fbapb='echo "running flutter build appbundle --release"; flutter build appbundle --release'
-
-alias fc='echo "running flutter clean"; flutter clean'
-
-alias df='echo "running dart format ."; dart format .'
-
-alias fgl='echo "running flutter gen-l10n"; flutter gen-l10n'
-##### Useful Flutter commands aliases #####
+# FVM aliases using the same core commands
+alias fdrbrb='run_command "$fvm_prefix$CMD_DRBRB"'
+alias fdrbrw='run_command "$fvm_prefix$CMD_DRBRW"'
+alias ffpg='run_command "$fvm_prefix$CMD_FPG"'
+alias ffbapkr='run_command "$fvm_prefix$CMD_FBAPKR"'
+alias ffbapk64r='run_command "$fvm_prefix$CMD_FBAPK64R"'
+alias ffbab='run_command "$fvm_prefix$CMD_FBAB"'
+alias ffc='run_command "$fvm_prefix$CMD_FC"'
+alias fdf='run_command "$fvm_prefix$CMD_DF"'
+alias ffgl='run_command "$fvm_prefix$CMD_FGL"'
+alias ffcpggl='run_command "$fvm_prefix$CMD_FCPGGL"'
+alias ffrde='run_command "$fvm_prefix$CMD_FRDE"'
+alias fdfx='run_command "$fvm_prefix$CMD_DFX"'
+alias ffd='run_command "$fvm_prefix$CMD_FD"'
